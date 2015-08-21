d7f249c7f92cb94420ce2583704cd778bc4507c0
Julian Haeger <julianhaeger@gmail.com>
2015-08-21 17:39:13 +0100

    cleaner

--- lib/fleetbattlex/physics.ex
+++ lib/fleetbattlex/physics.ex
@@ -1,5 +1,10 @@
 defmodule Fleetbattlex.Physics do
-	def apply_velocity(_position = {x,y}, _accelleration = {dx,dy}, dt) do
-		{(x + dx * dt),(y + dy * dt)}
+	def apply_velocity(position, accelleration, dt) do
+		movement = scale_vector(accelleration, dt)
+		add_vectors(position, movement)
 	end
+
+	defp scale_vector({x,y}, scale), do: {x * scale, y * scale}
+
+	defp add_vectors({x,y}, {x2,y2}), do: {x + x2, y + y2}
 end
