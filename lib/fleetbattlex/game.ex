f3129f51df129fbe1214ee85f52d370d8bb82582
Julian Haeger <julianhaeger@gmail.com>
2015-09-15 17:45:50 +0100

    simplistic implementation of individual ship processes with no real concurrency

--- lib/fleetbattlex/game.ex
+++ lib/fleetbattlex/game.ex
@@ -3,39 +3,42 @@ defmodule Fleetbattlex.Game do
 	require Logger
 	alias Fleetbattlex.Massive
 	alias Fleetbattlex.Physics
+	alias Fleetbattlex.Ship
 
 	def start_link() do
 		Logger.info "in game start_link"
 		GenServer.start_link(__MODULE__,%{pieces: [
 			%Massive{velocity: {0,8}, position: {50,0}},
 			%Massive{velocity: {0.0, 0.0}, position: {100,0}, mass: 25}
-		], ships: Fleetbattlex.Ship.start_link(%Massive{velocity: {0,10}, position: {40,0}})})
+		], ships: [
+			Ship.start_link(%Massive{velocity: {0,8}, position: {50,0}}),
+			Ship.start_link(%Massive{velocity: {0.0, 0.0}, position: {100,0}, mass: 25})
+		] |> Enum.map(fn {:ok, pid} -> pid end)})
 	end
 
 	def init(args) do
-		seconds = 0.5
+		seconds = 0.1
 		:erlang.send_after(trunc(seconds * 1000), self(), :game_tick)
-		{:ok, args}
+		{:ok, initialise_positions(args)}
 	end
 
-	def handle_info(:game_tick, state = %{pieces: pieces }) do
+	def handle_info(:game_tick, state = %{ships: ships, last_positions: last_positions }) do
 		Logger.info "tick"
 
 		seconds = 0.1
 
-		gravity_for_each = pieces |> Enum.map(&gravity_from_others(&1, pieces))
+		gravity_for_each = last_positions |> Enum.map(&gravity_from_others(&1, last_positions))
 		Logger.info "gravity for each = #{inspect gravity_for_each}"
-		pieces_with_gravity = Enum.zip(pieces,gravity_for_each)
+		ships_with_gravity = Enum.zip(ships,gravity_for_each)
 
-		updated_pieces = pieces_with_gravity |> Enum.map(fn {piece, gravity} -> Massive.progress_for_time(piece, seconds, [gravity]) end)
-		Logger.info "pieces = #{inspect updated_pieces}"
+		updated_posititions = ships_with_gravity |> Enum.map(fn {ship, gravity} -> Ship.progress_for_time(ship, seconds, [gravity]) end)
+		Logger.info "updated_posititions = #{inspect updated_posititions}"
 
-		positions = updated_pieces |> Enum.map(fn %{position: {x,y}} -> %{position: %{x: x, y: y}} end)
-		Fleetbattlex.Endpoint.broadcast! "positions:updates", "update", %{positions: positions}
+		position_update_message = updated_posititions |> Enum.map(fn %{position: {x,y}} -> %{position: %{x: x, y: y}} end)
+		Fleetbattlex.Endpoint.broadcast! "positions:updates", "update", %{positions: position_update_message}
 
-		
 		:erlang.send_after(trunc(seconds * 1000), self(), :game_tick)
-		{:noreply, %{state | pieces: updated_pieces}}
+		{:noreply, %{state | last_positions: updated_posititions}}
 	end
 
 	defp gravity_from_others(target = %{position: position, mass: mass}, others) do
@@ -46,5 +49,11 @@ defmodule Fleetbattlex.Game do
 			|> Enum.map(&Physics.calculate_gravitational_field(target, &1))
 			# |> Enum.map(fn {x,y} -> {-x,-y}end)
 			|> Physics.sum_vectors
+	end
+
+	defp initialise_positions(state)do
+		Logger.info "initialising positions for #{inspect state.ships}"
+		positions = state.ships |> Enum.map(fn ship -> Ship.current_position(ship) end)
+		Dict.put(state,:last_positions, positions)
 	end
 end
--- lib/fleetbattlex/ship.ex
+++ lib/fleetbattlex/ship.ex
@@ -15,10 +15,20 @@ defmodule Fleetbattlex.Ship do
 		GenServer.call(ship,{:progress_for_time,time,forces})
 	end
 
-	def handle_call({:progress_for_time, time, forces}, state = %{massive: massive}) do
+	def current_position(ship) do
+		GenServer.call(ship, {:current_position})
+	end
+
+
+	def handle_call({:progress_for_time, time, forces}, _from, state = %{massive: massive}) do
 		updated_massive = Massive.progress_for_time(massive,time,forces)
 		summary = %{position: updated_massive.position, mass: updated_massive.mass}
 		{:reply, summary, %{state | massive: updated_massive}}
 	end
 
+	def handle_call({:current_position}, _from, state = %{massive: massive}) do
+		Logger.info "in current position"
+		current_position = %{position: massive.position, mass: massive.mass}
+		{:reply, current_position, state}
+	end
 end
