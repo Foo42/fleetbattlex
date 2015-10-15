c65dfbbdc8c50407de11e6d8ff44f2a5d564dc73
Julian Haeger <julianhaeger@gmail.com>
2015-10-15 17:10:09 +0100

    fix for torpedos always pointing the same direction when launched

--- lib/fleetbattlex/ship.ex
+++ lib/fleetbattlex/ship.ex
@@ -67,7 +67,7 @@ defmodule Fleetbattlex.Ship do
 	def handle_call({:fire_torpedo, tube_number}, _from, state = %{name: {fleet_name, ship_name}, bearing: bearing, massive: ship_massive}) do
 		torpedo_name = {fleet_name, tube_number}
 		torpedo_position = ship_massive.position |> Physics.sum_vectors(Physics.scale_vector(bearing,40))
-		torpedo_params = %{name: torpedo_name, massive: %{ship_massive | mass: 0.2, position: torpedo_position}}
+		torpedo_params = %{name: torpedo_name, bearing: bearing, massive: %{ship_massive | mass: 0.2, position: torpedo_position}}
 		Fleetbattlex.Torpedo.start_link(torpedo_params)
 		spawn_link fn -> Fleetbattlex.Game.add_piece(torpedo_name) end
 		{:reply, torpedo_name, state}
